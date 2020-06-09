0170e8a6dc5d3b397c1ff5673a3ca6248d3ca1e7
shan <1132649527@qq.con>
2020-06-09 10:45:13 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 按需导入element-ui
+import '../src/plugins/element'
+// 引入axios
+import axios from 'axios'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
